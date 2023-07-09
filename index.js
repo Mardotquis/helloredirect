93f2767ba866d2142af758b44038e276f21419da
Marquis Mitchell <marquis0403@gmail.com>
2023-07-09 03:51:53 +0000

    another fix attempt

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 const app = require('./app'); // Replace with the path to your Express app file
 const express = require('express');
+const serverless = require('serverless-http');
 
-// Define your routes and middleware
-app.get('/', (req, res) => {
-  res.send('Hello, q world!');
-});
+// Create the Lambda handler function
+const handler = serverless(app);
 
-// Export the handler function
 module.exports.handler = async (event, context) => {
-  return new Promise((resolve, reject) => {
-    // Run the Express application
-    app(event, context, (error) => {
-      if (error) {
-        console.log('error occ::', error)
-        reject(error);
-      } else {
-        resolve();
-      }
-    });
-  });
+  try {
+    // You can do other things here
+
+    const result = await handler(event, context);
+
+    // You can do other things here
+
+    return result;
+  } catch (error) {
+    // Log the error
+    console.error('An error occurred:', error);
+
+    // Return an error response if needed
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Oh fuck. Internal Server Error' }),
+    };
+  }
 };
